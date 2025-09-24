78130c439eed24996bda4f6302761365f4bb5dae
FistoAR <FistoAR>
2025-09-24 09:45:48 +0530

    first commit

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ ScrollSmoother.create({
 });
 
 const cont = document.querySelector(".panel-container");
-const panels = gsap.utils.toArray(".panel-container .panel");
+const panel = gsap.utils.toArray(".panel-container .panel");
 
-const horizontalTween = gsap.to(panels, {
+const horizontalTween = gsap.to(panel, {
   x: () => -1 * (cont.scrollWidth - innerWidth),
   ease: "none",
   scrollTrigger: {
@@ -84,18 +84,6 @@ ScrollTrigger.create({
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // wheel code 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -135,6 +123,51 @@ images.forEach((img, i) => {
     .set(img, { display: "none", opacity: 0, visibility: "hidden" });            // instantly hide image
 });
 
+// ------------------------------------------------------------------------------- 
+
+
+
+
+
+
+ 
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Pin first section
+  ScrollTrigger.create({
+    trigger: ".static-1",
+    start: "top top",
+    end: "+=300",   // adjust how long it stays pinned
+    pin: true,
+    // pinSpacing: true
+  });
+
+  // Pin video section
+  ScrollTrigger.create({
+    trigger: "#videoSection",
+    start: "top top",
+    end: "+=300",   // change value based on how long you want it pinned
+    pin: true,
+    // pinSpacing: true
+  });
+
+  ScrollTrigger.create({
+    trigger: "#FAQ_section",
+    start: "top top",
+    end: "+=300",   // change value based on how long you want it pinned
+    pin: true,
+    // pinSpacing: true
+  });
+
+
+ ScrollTrigger.create({
+    trigger: "#model-section2",
+    start: "top top",
+    end: "+=300",   // change value based on how long you want it pinned
+    pin: true,
+    // pinSpacing: true
+  });
 
 
 
@@ -142,129 +175,52 @@ images.forEach((img, i) => {
 
 
 
-// const sections = document.querySelectorAll("section");
-// const images1 = document.querySelectorAll(".bg");
-// const headings = gsap.utils.toArray(".section-heading");
-// const outerWrappers = gsap.utils.toArray(".outer");
-// const innerWrappers = gsap.utils.toArray(".inner");
-// const clamp = gsap.utils.clamp(0, sections.length - 1);
 
-// document.addEventListener("wheel", handleWheel);
-// document.addEventListener("touchstart", handleTouchStart);
-// document.addEventListener("touchmove", handleTouchMove);
-// document.addEventListener("touchend", handleTouchEnd);
 
-// let animating = false,
-//   currentIndex = -1;
 
-// const touch = {
-//   startX: 0,
-//   startY: 0,
-//   dx: 0,
-//   dy: 0,
-//   startTime: 0,
-//   dt: 0
-// };
 
-// const tlDefaults = {
-//   ease: "slow.inOut",
-//   duration: 1.25
-// };
 
-// const splitHeadings = headings.map((heading) => {
-//   return new SplitText(heading, {
-//     type: "chars, words, lines",
-//     linesClass: "clip-text"
-//   });
-// });
+//  gsap.registerPlugin(ScrollTrigger);
 
-// gsap.set(outerWrappers, { yPercent: 100 });
-// gsap.set(innerWrappers, { yPercent: -100 });
+//   let panells = gsap.utils.toArray("section");
 
-// function gotoSection(index, direction) {
-//   index = clamp(index); // make sure it's valid
-
-//   // we're at the end, so exit
-//   if (currentIndex === index) {
-//     return;
-//   }
-
-//   animating = true;
-//   let fromTop = direction === -1,
-//     dFactor = fromTop ? -1 : 1,
-//     tl = gsap.timeline({
-//       defaults: { duration: 1.25, ease: "power1.inOut" },
-//       onComplete: () => (animating = false)
-//     });
-//   if (currentIndex >= 0) {
-//     // The first time this function runs, current is -1
-//     gsap.set(sections[currentIndex], { zIndex: 0 });
-//     tl.to(images1[currentIndex], { yPercent: -15 * dFactor }).set(
-//       sections[currentIndex],
-//       { autoAlpha: 0 }
-//     );
-//   }
-//   gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
-//   tl.fromTo(
-//     [outerWrappers[index], innerWrappers[index]],
-//     { yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor) },
-//     { yPercent: 0 },
-//     0
-//   )
-//     .fromTo(images1[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
-//     .fromTo(
-//       splitHeadings[index].chars,
-//       { autoAlpha: 0, yPercent: 150 * dFactor },
-//       {
+//   panells.forEach((panell, i) => {
+//     gsap.fromTo(panell, 
+//       { autoAlpha: 0 }, 
+//       { 
 //         autoAlpha: 1,
-//         yPercent: 0,
-//         duration: 1,
-//         ease: "power2",
-//         stagger: {
-//           each: 0.02,
-//           from: "random"
+//         duration: 0.5,
+//         ease: "none",
+//         scrollTrigger: {
+//           trigger: panell,
+//           start: "top center",   // when section reaches middle
+//           end: "bottom center",  // fade out as you scroll past
+//           toggleActions: "play reverse play reverse", // fade in/out
 //         }
-//       },
-//       0.2
+//       }
 //     );
-
-//   currentIndex = index;
-// }
-
-// function handleWheel(e) {
-//   if (animating) return;
-
-//   e.wheelDeltaY < 0
-//     ? gotoSection(currentIndex + 1, 1)
-//     : gotoSection(currentIndex - 1, -1);
-// }
-
-// function handleTouchStart(e) {
-//   const t = e.changedTouches[0];
-//   touch.startX = t.pageX;
-//   touch.startY = t.pageY;
-// }
-
-// function handleTouchMove(e) {
-//   e.preventDefault();
-// }
-
-// function handleTouchEnd(e) {
-//   if (animating) return;
-//   const t = e.changedTouches[0];
-//   touch.dx = t.pageX - touch.startX;
-//   touch.dy = t.pageY - touch.startY;
-//   if (touch.dy > 10) gotoSection(currentIndex - 1, -1);
-//   if (touch.dy < -10) gotoSection(currentIndex + 1, 1);
-// }
-
-// gotoSection(0, 1);
+//   });
 
 
 
+//  gsap.registerPlugin(ScrollTrigger);
 
+//   let panells = gsap.utils.toArray(".panell");
 
+//   // hide all except first
+//   gsap.set(panells.slice(1), { display: "none" });
 
-
-
-
+//   panells.forEach((panell, i) => {
+//     ScrollTrigger.create({
+//       trigger: panell,
+//       start: "top center",
+//       onEnter: () => {
+//         gsap.set(panells, { display: "none" });
+//         gsap.set(panell, { display: "block" });
+//       },
+//       onEnterBack: () => {
+//         gsap.set(panells, { display: "none" });
+//         gsap.set(panell, { display: "block" });
+//       }
+//     });
+//   });
